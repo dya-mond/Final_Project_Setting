f20f51a490e015a3cb50ad0a3296d0e409bcf898
강용수 <97223877+yongsu-kang@users.noreply.github.com>
2022-07-23 16:35:29 +0900

    Add files via upload

---
+++ final_setting/src/main/java/com/example/final_setting/FinalSettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalSettingApplication.class, args);
+    }
+
+}
---
+++ final_setting/src/test/java/com/example/final_setting/FinalSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
